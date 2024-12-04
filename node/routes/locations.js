83b728c837bb25be5100e7e626d87a2adedd7f59
DiteErick <za2012467@zapopan.tecmm.edu.mx>
2024-12-03 19:08:15 -0600

    minor fixes

--- node/routes/locations.js
+++ node/routes/locations.js
@@ -3,12 +3,12 @@ import * as locationController from '../controllers/locationController.js';
 
 const router = express.Router();
 
+//aqui filtra los equipos por parametros en la barra de busqueda
+router.get('/search', locationController.searchAssets);
 // aqui se arma el exel con toda la informacion
 router.get('/details', locationController.getLocationsWithDetails);
 // aqui se arma el exel con la informacion de una locacion especifica
 router.get('/details/:id', locationController.getLocationDetailsById);
-//aqui filtra los equipos por parametros en la barra de busqueda
-router.get('/search', locationController.searchAssets);
 
 router.get('/', locationController.getAllLocations);
 router.post('/', locationController.createLocation);
